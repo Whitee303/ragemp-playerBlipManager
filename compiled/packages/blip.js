56bf53fe8588a933d8e816569bc9cd6a99dcd39e
Whitee <84782812+Whitee303@users.noreply.github.com>
2025-11-05 21:03:20 +0100

    Fix blip sync call and update positions regularly

--- compiled/packages/blip.js
+++ compiled/packages/blip.js
@@ -17,7 +17,9 @@ var server = {
                 name: player.name
             }
         ]);
-    // player.call('blips:syncEvery', [blips]);
+        player.call("blips:syncEvery", [
+            blips
+        ]);
     },
     playerQuit: function playerQuit(player) {
         mp.players.forEach(function(p) {
@@ -28,8 +30,25 @@ var server = {
             ]);
         });
         delete blips[player.id];
+    },
+    updatePositions: function updatePositions() {
+        mp.players.forEach(function(p) {
+            p.call("blips:updateBlips", [
+                {
+                    id: p.id,
+                    pos: {
+                        x: p.position.x,
+                        y: p.position.y,
+                        z: p.position.z
+                    }
+                }
+            ]);
+        });
     }
 };
 mp.events.add("blips:syncer", server.sync);
 mp.events.add("playerJoin", server.playerJoin);
 mp.events.add("playerQuit", server.playerQuit);
+setInterval(function() {
+    server.updatePositions();
+}, 300);
