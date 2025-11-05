fc76b18138c7a6a45d280c4e3a0135c9440cdb2e
Whitee <84782812+Whitee303@users.noreply.github.com>
2025-11-05 21:01:39 +0100

    Update blip.ts
    Added blip position updateing

--- packages/blip.ts
+++ packages/blip.ts
@@ -1,12 +1,13 @@
 interface BlipContainer {
-	[key: number]: BlipMp;
+	[key: number]: BlipMp
 }
 interface ServerModule {
 	sync: (player: PlayerMp, blip: string) => void;
 	playerJoin: (player: PlayerMp) => void;
 	playerQuit: (player: PlayerMp) => void;
+	updatePositions: () => void;
 }
-const blips: BlipContainer = {};
+const blips:BlipContainer = {};
 const server: ServerModule = {
 	sync (_: PlayerMp, blip: string) {
 		const unpackedJSON = JSON.parse(blip);
@@ -17,15 +18,30 @@ const server: ServerModule = {
 	},
 	playerJoin (player: PlayerMp) {
 		player.call('blips:callCreateBlip', [{ id: player.id, name: player.name }]);
-		// player.call('blips:syncEvery', [blips]);
+		player.call('blips:syncEvery', [blips]);
 	},
 	playerQuit (player: PlayerMp) {
 		mp.players.forEach((p) => {
 			p.call('blips:remove', [{ id: player.id }]);
 		});
 		delete blips[player.id];
+	},
+	updatePositions () {
+		mp.players.forEach((p) => {
+			p.call('blips:updateBlips', [{
+				id: p.id, pos: {
+					x: p.position.x,
+					y: p.position.y,
+					z: p.position.z
+				}
+			}]);
+		});
 	}
 };
 mp.events.add('blips:syncer', server.sync);
 mp.events.add('playerJoin', server.playerJoin);
 mp.events.add('playerQuit', server.playerQuit);
+
+setInterval(() => {
+	server.updatePositions();
+}, 300);
